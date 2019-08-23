554ef3981e643af7faa7d5a880eec10505ae80a4
Angingun <46560915+Angingun@users.noreply.github.com>
2019-08-23 13:38:55 +0800

    realize freq_output

--- excel_output_terms.py
+++ excel_output_terms.py
@@ -1,4 +1,5 @@
 from nltk.corpus import stopwords, PlaintextCorpusReader
+from nltk.probability import FreqDist
 import nltk
 import xlwings as xw
 
@@ -20,8 +21,16 @@ class CoporaHandler(object):
         stop_words = stopwords.words('english')
         n1 = [word.lower() for word in self._text if word.isalpha()]
         n2 = [word for word in n1 if word not in stop_words]
-        n3 = sorted(list(set(n2)))
-        return n3
+        # n3 = sorted(list(set(n2)))
+        return n2
+
+    def freq_list(self):
+        fdist = FreqDist(self.nor_copora)
+        word_list = list(fdist.keys())
+        count_list = list(fdist.values())
+        pair = zip(word_list, count_list)
+        pair_freq = sorted(pair, key=lambda max: -max[1])
+        return pair_freq
 
 
 def upload_corpus(root, file):
@@ -33,7 +42,8 @@ def output_excel(terms, filepath):
     app = xw.App(visible=False, add_book=False)
     wb = app.books.add()
     sht1 = wb.sheets('sheet1')
-    sht1.range('A1').options(transpose=True).value = terms
+    # sht1.range('A1').options(transpose=True).value = terms
+    sht1.range('A1').options(expand='table').value = terms
     wb.save(filepath)
     wb.close()
 
@@ -43,8 +53,8 @@ def main():
     terms = CoporaHandler()
     terms.text = copora
     output_excel(
-        terms.nor_copora,
-        r'C:\Users\acer\Desktop\corpus _python\xlsx_test\excel_output_terms.xlsx'
+        terms.freq_list(),
+        r'C:\Users\acer\Desktop\corpus _python\xlsx_test\excel_output_freq.xlsx'
     )
 
 
